4668542aeeea173f910440b1a355bc1cf1e2e7a9
chaksack <chakdahah@gmail.com>
2025-06-01 00:48:22 +0100

    ui updates

---
+++ frontend/app.vue
@@ -0,0 +1,12 @@
+<template>
+  <div> <NuxtLayout>
+    <NuxtLoadingIndicator />
+    <NuxtPage />
+  </NuxtLayout>
+    <Toaster richColors position="top-right" /> </div>
+</template>
+
+<script setup lang="ts">
+import { Toaster, toast } from 'vue-sonner'
+import 'vue-sonner/style.css'
+</script>
