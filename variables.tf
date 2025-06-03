582466ba7c0c14af8e3d1d9dd8d0f0497fd80d1f
chaksack <chakdahah@gmail.com>
2025-06-03 16:03:05 +0100

    project_name update

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "A short name for the project, used for naming resources"
   type        = string
-  default     = "invoiceapp"
+  default     = "invoice_demo"
 }
 
 variable "vpc_cidr" {
