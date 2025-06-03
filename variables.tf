75635eca1be093486f91ce4fc0293d78ebdb69e7
chaksack <chakdahah@gmail.com>
2025-06-03 16:21:32 +0100

    s3 name

--- variables.tf
+++ variables.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "A short name for the project, used for naming resources"
   type        = string
-  default     = "invoice_demo"
+  default     = "invoicedemo"
 }
 
 variable "vpc_cidr" {
