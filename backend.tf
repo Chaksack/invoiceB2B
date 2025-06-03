45c0c87fe334c4b9cc1a0000b77015a6be1c31ed
chaksack <chakdahah@gmail.com>
2025-06-03 16:19:02 +0100

    dynamo name

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-terraform {
-  backend "s3" {
-    bucket         = "invoicefin-terraform-state"
-    key            = "terraform.tfstate"  # Default state file path, override with -backend-config
-    region         = "us-east-1"
-    encrypt        = true
-    dynamodb_table = "${var.project_name}-terraform-locks"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "${var.project_name}-terraform-state"
+#     key            = "terraform.tfstate"  # Default state file path, override with -backend-config
+#     region         = "us-east-1"
+#     encrypt        = true
+#     dynamodb_table = "${var.project_name}-terraform-locks"
+#   }
+# }
