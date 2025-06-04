3e9ca147046c7946d71dfed4acccc6f6510c862d
chaksack <chakdahah@gmail.com>
2025-06-04 01:30:32 +0100

    update5

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,11 @@ output "ecr_api_repository_url" {
   value       = aws_ecr_repository.api.repository_url
 }
 
+output "api_repository_url" {
+  description = "URL of the API ECR repository (alias for ecr_api_repository_url)"
+  value       = aws_ecr_repository.api.repository_url
+}
+
 output "ecs_cluster_name_output" {
   description = "Name of the ECS cluster"
   value       = aws_ecs_cluster.main.name
