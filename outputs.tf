b95ea22de11ad368ef45c095b016e7e9afedc4d2
chaksack <chakdahah@gmail.com>
2025-06-04 01:56:44 +0100

    update7

--- outputs.tf
+++ outputs.tf
@@ -28,9 +28,14 @@ output "api_repository_url" {
   value       = aws_ecr_repository.api.repository_url
 }
 
-output "ecs_cluster_name_output" {
+output "ecs_cluster_name" {
   description = "Name of the ECS cluster"
   value       = aws_ecs_cluster.main.name
+}
+
+output "api_service_name" {
+  description = "Name of the API service in ECS"
+  value       = "${var.project_name}-api-service"
 }
 
 
